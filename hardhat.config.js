137dc9a69d6b89683bdf2a34178942df705ea64e
PrinceRatheee <rathi.prince2@gmail.com>
2024-10-12 02:43:32 +0530

    project done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 // hardhat.config.js
 
 require("@nomicfoundation/hardhat-toolbox"); // Add this line at the top
-
+require('dotenv').config();
 module.exports = {
   solidity: "0.8.24",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/3CCw6ZCYuOR9AROnGhajCveCXm28tjdV",
-      accounts: ['0x93cb4503d0b70e2cca9f1d9046d6fb2a3eb7be464dbf1ffce0f5c600a51e530c'],
+      url: process.env.SEPOLIA_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
   paths: {
